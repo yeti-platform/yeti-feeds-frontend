8b77c9b4d00ab6ca8808f341a1715de4c6bc9c30
Thomas Chopitea <tomchop@gmail.com>
2020-04-12 16:28:40 +0200

    Initial commit

---
+++ src/views/FeedList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    Feed list!
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FeedList"
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    Homepage
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home"
+};
+</script>
