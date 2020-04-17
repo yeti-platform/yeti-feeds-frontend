2ec41ac1784824f3bfc0a908ebcb52e047b15ae9
Thomas Chopitea <tomchop@gmail.com>
2020-04-17 23:07:47 +0200

    TagInput component

---
+++ src/components/TagInput.vue
@@ -0,0 +1,60 @@
+<template>
+  <b-taginput
+    label="tags"
+    v-model="selectedTags"
+    :data="filterTags"
+    autocomplete
+    icon="tag"
+    placeholder="e.g. Zeus"
+    field="name"
+  >
+  </b-taginput>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "TagSelector",
+  data() {
+    return {
+      existingTags: [],
+      tagName: "",
+      selectedTags: []
+    };
+  },
+  props: ["value"],
+  mounted() {
+    this.getExistingTags();
+  },
+  methods: {
+    getExistingTags() {
+      axios
+        .get("http://localhost:5000/api/tag/")
+        .then(response => {
+          return (this.existingTags = response.data);
+        })
+        .catch(error => {
+          return console.log(error);
+        });
+    },
+    updateSelected() {
+      this.$emit("input", this.selectedTags);
+    }
+  },
+  computed: {
+    filterTags() {
+      return this.existingTags.filter(tag => {
+        return (
+          tag.name
+            .toString()
+            .toLowerCase()
+            .indexOf(this.tagName.toLowerCase()) >= 0
+        );
+      });
+    }
+  },
+  watch: {
+    selectedTags: "updateSelected"
+  }
+};
+</script>
