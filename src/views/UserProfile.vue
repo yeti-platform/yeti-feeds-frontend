ebf6aec3402ee16eeb347aec697f2612993bb807
Thomas Chopitea <tomchop@gmail.com>
2020-05-29 22:47:50 +0200

    Edit settings in profile

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,62 +1,84 @@
 <template>
-  <div class="columns">
-    <div class="column is-7">
-      <div class="card">
-        <header class="card-header is-info">
-          <p class="card-header-title">
-            Profile info
-          </p>
-        </header>
-        <div class="card-content">
-          <table class="table is-fullwidth" v-if="profile">
-            <tbody>
-              <tr>
-                <th>Username</th>
-                <td>{{ profile.username }}</td>
-                <td></td>
-              </tr>
-              <tr>
-                <th>API key</th>
-                <td>
-                  <code>{{ profile.api_key }}</code>
-                </td>
-                <td>
-                  <b-button class="button is-outline reset-button" size="is-small" @click="resetApiKey(profile)">
-                    <span>Reset key</span>
-                  </b-button>
-                </td>
-              </tr>
-              <tr>
-                <th>Groups</th>
-                <td>
-                  <b-taglist>
-                    <b-tag type="is-info" v-bind:key="group.groupname" v-for="group in profile.groups">
-                      {{ group.groupname }}
-                    </b-tag>
-                  </b-taglist>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+  <div>
+    <div class="columns">
+      <div class="column is-7">
+        <div class="card">
+          <header class="card-header is-info">
+            <p class="card-header-title">
+              Profile info
+            </p>
+          </header>
+          <div class="card-content">
+            <table class="table is-fullwidth" v-if="profile">
+              <tbody>
+                <tr>
+                  <th>Username</th>
+                  <td>{{ profile.username }}</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <th>API key</th>
+                  <td>
+                    <code>{{ profile.api_key }}</code>
+                  </td>
+                  <td>
+                    <b-button class="button is-outline reset-button" size="is-small" @click="resetApiKey(profile)">
+                      <span>Reset key</span>
+                    </b-button>
+                  </td>
+                </tr>
+                <tr>
+                  <th>Groups</th>
+                  <td>
+                    <b-taglist>
+                      <b-tag type="is-info" v-bind:key="group.groupname" v-for="group in profile.groups">
+                        {{ group.groupname }}
+                      </b-tag>
+                    </b-taglist>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="column is-5">
+        <div class="card">
+          <header class="card-header is-warning">
+            <p class="card-header-title">
+              Change password
+            </p>
+          </header>
+          <div class="card-content">
+            <b-field>
+              <b-input v-model="currentPassword" type="password" placeholder="Current password"></b-input>
+            </b-field>
+            <b-field>
+              <b-input v-model="newPassword" type="password" placeholder="New password" password-reveal> </b-input>
+            </b-field>
+            <p class="control"><b-button type="is-primary" @click="changeUserPassword()">Save</b-button></p>
+          </div>
         </div>
       </div>
     </div>
-    <div class="column is-5">
-      <div class="card">
-        <header class="card-header is-warning">
-          <p class="card-header-title">
-            Change password
-          </p>
-        </header>
-        <div class="card-content">
-          <b-field>
-            <b-input v-model="currentPassword" type="password" placeholder="Current password"></b-input>
-          </b-field>
-          <b-field
-            ><b-input v-model="newPassword" type="password" placeholder="New password" password-reveal></b-input
-          ></b-field>
-          <p class="control"><b-button type="is-primary" @click="changeUserPassword()">Save</b-button></p>
-        </div>
+    <div class="columns">
+      <div class="column" v-if="profile">
+        <p class="title is-4">Miscellaneous settings</p>
+        <p class="subtitle is-6">
+          Any Yeti object can register per-user settings for specific tweaking. This is especially useful for API keys
+          or individual credentials.
+        </p>
+        <b-field
+          v-for="setting_name in Object.keys(profile.available_settings)"
+          v-bind:key="profile.available_settings[setting_name].name"
+          :label="profile.available_settings[setting_name].name"
+        >
+          <b-input
+            :placeholder="profile.available_settings[setting_name].description"
+            v-model="profile.settings[setting_name]"
+          ></b-input>
+        </b-field>
+        <p class="control"><b-button type="is-primary" @click="saveUserSettings()">Save settings</b-button></p>
       </div>
     </div>
   </div>
@@ -71,7 +93,8 @@ export default {
     return {
       profile: null,
       currentPassword: null,
-      newPassword: null
+      newPassword: null,
+      availableSettings: []
     };
   },
   methods: {
@@ -80,6 +103,7 @@ export default {
         .get(`/api/users`)
         .then(response => {
           this.profile = response.data;
+          //   this.availableSettings = response.data.availableSettings.keys();
         })
         .catch(error => {
           console.log(error);
@@ -124,6 +148,20 @@ export default {
           this.currentPassword = null;
           this.newPassword = null;
         });
+    },
+    saveUserSettings() {
+      axios
+        .post(`/api/users/settings`, this.profile.settings)
+        .then(() => {
+          this.$buefy.notification.open({
+            message: `Settings successfully updated.`,
+            type: "is-success"
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        })
+        .finally(() => {});
     }
   },
   mounted() {
