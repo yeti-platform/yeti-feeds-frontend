6f1f2829b1a427dba72881404e1a9c9a0839c4b2
Thomas Chopitea <tomchop@gmail.com>
2022-12-12 19:37:33 +0100

    Add docker things

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+if [[ "$1" =~ "dev" ]]; then
+    if [[ ! -f ".node_installed" ]]; then
+      echo ".node_installed not found, installing node modules"
+      npm install
+      touch .node_installed
+    fi
+    if [[ ! -z "${VUE_API_BACKEND}" ]]; then
+      # this is the busybox version of sed
+      sed -i s/localhost/${VUE_API_BACKEND}/ vue.config.js
+    fi
+    npm run serve
+fi
+
+exec "$@"
