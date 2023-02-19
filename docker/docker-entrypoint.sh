fc88154f0db49d0b33c7cc31c6b847cb8732102a
Thomas Chopitea <tomchop@gmail.com>
2023-02-19 10:26:57 +0000

    remove node modules if it exists

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 if [[ "$1" =~ "dev" ]]; then
     if [[ ! -f ".node_installed" ]]; then
       echo ".node_installed not found, installing node modules"
+      rm -rf node_modules
       npm install
       touch .node_installed
     fi
