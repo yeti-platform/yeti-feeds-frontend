e3ea3d5856813ef667f5816105fb4e7a7a5863d3
Sebdraven <sebdraven@protonmail.com>
2023-08-15 18:43:15 +0200

    Update observableDefinitions.js
    add definitions

--- src/definitions/observableDefinitions.js
+++ src/definitions/observableDefinitions.js
@@ -28,5 +28,47 @@ export const OBSERVABLE_TYPES = [
       { field: "description", type: "longtext", label: "Description", displayList: false }
     ],
     icon: "globe"
-  }
+  },
+  {
+    name:"file",
+    type:"file",
+    fields:[
+      { field: "value", type: "text", label: "Value", displayList: true },
+      { field: "tags", type: "list", label: "tags", displayList: true },
+      { field: "description", type: "longtext", label: "Description", displayList: false }
+    ],
+    icon: "file"
+
+  },
+  {
+    name: "sha256",
+    type: "sha256",
+    fields: [
+      { field: "value", type: "text", label: "Value", displayList: true },
+      { field: "tags", type: "list", label: "tags", displayList: true },
+      { field: "description", type: "longtext", label: "Description", displayList: false }
+    ],
+    icon: "lock"
+  },
+  {
+    name: "md5",
+    type: "md5",
+    fields: [
+      { field: "value", type: "text", label: "Value", displayList: true },
+      { field: "tags", type: "list", label: "tags", displayList: true },
+      { field: "description", type: "longtext", label: "Description", displayList: false }
+    ],
+    icon: "lock"
+  },
+  {
+    name: "sha1",
+    type: "sha1",
+    fields: [
+      { field: "value", type: "text", label: "Value", displayList: true },
+      { field: "tags", type: "list", label: "tags", displayList: true },
+      { field: "description", type: "longtext", label: "Description", displayList: false }
+    ],
+    icon: "lock"
+  },
+
 ];
