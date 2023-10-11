8140d8c5800dd5e4c159e79d4564bed1ea1445ad
Thomas Chopitea <tomchop@gmail.com>
2023-10-11 09:02:03 +0000

    indicatorDefinitions update

--- src/definitions/indicatorDefinitions.js
+++ src/definitions/indicatorDefinitions.js
@@ -2,38 +2,75 @@ export const DIAMOND_MODEL = ["adversary", "capability", "infrastructure", "vict
 
 export const INDICATOR_TYPES = [
   {
-    name: "Regular expression",
+    name: "Regular expressions",
     type: "regex",
     fields: [
-      { field: "name", type: "text", label: "Name", displayList: true },
-      { field: "pattern", type: "code", label: "Pattern", displayList: true },
-      { field: "location", type: "text", label: "Location", displayList: true },
-      { field: "diamond", type: "option", choices: DIAMOND_MODEL, label: "Diamond model", displayList: true },
-      { field: "description", type: "longtext", label: "Description", displayList: false }
+      { field: "name", type: "text", label: "Name", displayList: true, displayInfoBox: true },
+      { field: "pattern", type: "code", label: "Pattern", displayList: false, displayInfoBox: true },
+      { field: "relevant_tags", type: "list", label: "Relevant tags", displayList: true, displayInfoBox: true },
+      { field: "location", type: "text", label: "Location", displayList: true, displayInfoBox: true },
+      {
+        field: "diamond",
+        type: "option",
+        choices: DIAMOND_MODEL,
+        label: "Diamond model",
+        displayList: true,
+        displayInfoBox: true
+      },
+      { field: "description", type: "longtext", label: "Description", displayList: false, displayInfoBox: true }
     ],
-    icon: "bolt"
+    icon: "code"
   },
   {
-    name: "Yara rule",
+    name: "Queries",
+    type: "query",
+    fields: [
+      { field: "name", type: "text", label: "Name", displayList: true, displayInfoBox: true },
+      { field: "pattern", type: "longcode", label: "Rule text", displayList: false, displayInfoBox: true },
+      { field: "relevant_tags", type: "list", label: "Relevant tags", displayList: true, displayInfoBox: true },
+      { field: "query_type", type: "text", label: "Query type", displayList: true, displayInfoBox: true },
+      { field: "target_systems", type: "text", label: "Target system", displayList: true, displayInfoBox: true },
+      { field: "description", type: "longtext", label: "Description", displayList: false, displayInfoBox: true }
+    ],
+    icon: "database"
+  },
+  {
+    name: "Yara rules",
     type: "yara",
     fields: [
-      { field: "name", type: "text", label: "Name", displayList: true },
-      { field: "pattern", type: "longcode", label: "Rule text", displayList: true },
-      { field: "location", type: "text", label: "Location", displayList: true },
-      { field: "diamond", type: "option", choices: DIAMOND_MODEL, label: "Diamond model", displayList: true },
-      { field: "description", type: "longtext", label: "Description", displayList: false }
+      { field: "name", type: "text", label: "Name", displayList: true, displayInfoBox: true },
+      { field: "pattern", type: "longcode", label: "Rule text", displayList: false, displayInfoBox: true },
+      { field: "relevant_tags", type: "list", label: "Relevant tags", displayList: true, displayInfoBox: true },
+      { field: "location", type: "text", label: "Location", displayList: true, displayInfoBox: true },
+      {
+        field: "diamond",
+        type: "option",
+        choices: DIAMOND_MODEL,
+        label: "Diamond model",
+        displayList: true,
+        displayInfoBox: true
+      },
+      { field: "description", type: "longtext", label: "Description", displayList: false, displayInfoBox: true }
     ],
     icon: "bolt"
   },
   {
-    name: "Sigma rule",
+    name: "Sigma rules",
     type: "sigma",
     fields: [
-      { field: "name", type: "text", label: "Name", displayList: true },
-      { field: "pattern", type: "longcode", label: "Rule text", displayList: true },
-      { field: "location", type: "text", label: "Location", displayList: true },
-      { field: "diamond", type: "option", choices: DIAMOND_MODEL, label: "Diamond model", displayList: true },
-      { field: "description", type: "longtext", label: "Description", displayList: false }
+      { field: "name", type: "text", label: "Name", displayList: true, displayInfoBox: true },
+      { field: "pattern", type: "longcode", label: "Rule text", displayList: false, displayInfoBox: true },
+      { field: "relevant_tags", type: "list", label: "Relevant tags", displayList: true, displayInfoBox: true },
+      { field: "location", type: "text", label: "Location", displayList: true, displayInfoBox: true },
+      {
+        field: "diamond",
+        type: "option",
+        choices: DIAMOND_MODEL,
+        label: "Diamond model",
+        displayList: true,
+        displayInfoBox: true
+      },
+      { field: "description", type: "longtext", label: "Description", displayList: false, displayInfoBox: true }
     ],
     icon: "bolt"
   }
