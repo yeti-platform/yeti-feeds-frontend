c5b06e53bc75d022b697bcab198927a14c7690b4
tomchop <tomchop@gmail.com>
2023-11-19 12:29:54 +0000

    Add skeleton for ObservableDetails

--- yeti-frontend/src/views/ObservableDetails.vue
+++ yeti-frontend/src/views/ObservableDetails.vue
@@ -1,7 +1,85 @@
-<template> Details for {{ observable }}</template>
+<template>
+  <v-container fluid>
+    <v-row align="start" no-gutters>
+      <v-col>
+        <v-card class="ma-2" variant="flat">
+          <template v-slot:title>
+            <code>{{ observable?.value }}</code>
+          </template>
+          <template v-slot:subtitle>
+            <v-chip color="primary" :text="observable?.type" label></v-chip>
+          </template>
+          {{ observable?.context }}
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        <v-card title="Info" class="ma-2" variant="outlined">
+          <table>
+            <tbody>
+              <tr v-for="field in getObservableInfoFields()">
+                <th>{{ field.label }}</th>
+                <td>{{ observable[field.field] }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </v-card>
+        <v-card title="Tags" class="ma-2" variant="outlined">
+          <v-combobox
+            v-model="observableTags"
+            chips
+            clearable
+            multiple
+            variant="outlined"
+            density="compact"
+            :delimiters="[',', ' ', ';']"
+            prepend-inner-icon="mdi-tag"
+          >
+            <template v-slot:chip="tag"> <v-chip :text="tag.item.value" label/></template>
+            <template v-slot:append>
+              <v-btn variant="tonal" color="primary" class="me-2" @click="changeTags">Save</v-btn>
+            </template>
+          </v-combobox>
+        </v-card>
+        <v-card title="Available analytics" class="ma-2" variant="outlined">
+          more details
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-container fluid>
+        <v-card variant="flat">
+          <v-tabs v-model="activeTab" color="primary">
+            <v-tab value="one"><v-icon size="x-large">mdi-graph</v-icon>Related observables</v-tab>
+            <v-tab value="two"><v-icon size="x-large">mdi-brain</v-icon>Related entities</v-tab>
+            <v-tab value="three"><v-icon size="x-large">mdi-tag</v-icon>Tag relationships</v-tab>
+          </v-tabs>
+
+          <v-card-text>
+            <v-window v-model="activeTab">
+              <v-window-item value="one">
+                One
+              </v-window-item>
+
+              <v-window-item value="two">
+                Two
+              </v-window-item>
+
+              <v-window-item value="three">
+                Three
+              </v-window-item>
+            </v-window>
+          </v-card-text>
+        </v-card>
+      </v-container>
+    </v-row>
+  </v-container>
+</template>
 
 <script lang="ts" setup>
 import axios from "axios";
+
+import { ENTITY_TYPES } from "@/definitions/entityDefinitions.js";
+import { OBSERVABLE_TYPES } from "@/definitions/observableDefinitions.js";
 </script>
 
 <script lang="ts">
@@ -15,8 +93,9 @@ export default {
   data() {
     return {
       observable: null,
-      newTags: [],
-      activeTab: 0
+      observableTags: [],
+      activeTab: 0,
+      observableTypes: OBSERVABLE_TYPES
     };
   },
   methods: {
@@ -24,8 +103,9 @@ export default {
       axios
         .get(`/api/v2/observables/${this.id}`)
         .then(response => {
+          let tagNames: string[] = [];
           this.observable = response.data;
-          this.newTags = Object.keys(this.observable.tags);
+          this.observableTags = Object.keys(this.observable.tags);
           // Switch back to Context view when reloading the page.
           this.activeTab = 0;
         })
@@ -33,6 +113,13 @@ export default {
           console.log(error);
         })
         .finally();
+    },
+    getObservableTypeDefinition() {
+      return this.observableTypes.find(typeDef => typeDef.type === this.observable?.type);
+    },
+    getObservableInfoFields() {
+      const hideFields = ["value", "tags", "description"];
+      return this.getObservableTypeDefinition()?.fields.filter(field => !hideFields.includes(field.field));
     }
   },
   mounted() {
