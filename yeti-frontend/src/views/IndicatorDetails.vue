92feca53e6bf5c648b6a903838aec61d2ed264b3
tomchop <tomchop@gmail.com>
2023-11-30 21:29:13 +0000

    IndicatorDetails base

---
+++ yeti-frontend/src/views/IndicatorDetails.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ id }}
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "IndicatorDetails",
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped></style>
