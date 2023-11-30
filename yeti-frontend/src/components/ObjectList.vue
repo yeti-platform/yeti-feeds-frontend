d6ee140b83fd991185a7243ccb80e8a5256d2a6c
tomchop <tomchop@gmail.com>
2023-11-30 20:51:26 +0000

    Refactor entitylist to use an ObjectList component

--- yeti-frontend/src/components/ObjectList.vue
+++ yeti-frontend/src/components/ObjectList.vue
@@ -1,7 +1,132 @@
 <template>
-  <div>ObjectList</div>
+  <v-data-table-server
+    v-model:page="page"
+    :itemsLength="total"
+    :items-per-page="perPage"
+    :headers="headers"
+    density="compact"
+    :items="items"
+    @update:options="loadOjects"
+    :search="searchQuery"
+    :item-value="item => item.id"
+    hover
+    class="ma-5"
+  >
+    <template v-slot:item.name="{ item }">
+      <router-link
+        :to="{ name: 'EntityDetails', params: { id: item.id } }"
+        class="text-blue"
+        style="text-decoration:none"
+        >{{ item.name }}</router-link
+      >
+    </template>
+    <template v-slot:item.tags="{ item }">
+      <v-chip
+        v-for="name in Object.keys(item.tags)"
+        :color="item.tags[name].fresh ? 'blue ' : 'red'"
+        :text="name"
+        class="mx-1"
+        label
+        size="small"
+      ></v-chip>
+    </template>
+    <template v-slot:item.aliases="{ item }">
+      <v-chip v-for="value in item.aliases" :text="value" class="mx-1" label size="small"></v-chip>
+    </template>
+    <template v-slot:item.threat_actor_types="{ item }">
+      <v-chip v-for="value in item.threat_actor_types" :text="value" class="mx-1" label size="small"></v-chip>
+    </template>
+  </v-data-table-server>
 </template>
 
 <script lang="ts" setup>
-//
+import axios from "axios";
+import { ENTITY_TYPES } from "@/definitions/entityDefinitions.js";
+</script>
+
+<script lang="ts">
+export default {
+  name: "objectList",
+  props: {
+    searchQuery: {
+      type: String,
+      default: ""
+    },
+    searchType: {
+      type: String,
+      default: "entity"
+    },
+    searchSubtype: {
+      type: String,
+      default: ""
+    },
+    headers: {
+      type: Array,
+      default: () => [
+        { title: "Name", key: "name" },
+        { title: "Tags", key: "tags" },
+        { title: "Created on", key: "created" }
+      ]
+    },
+    checkable: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      items: [],
+      page: 1,
+      perPage: 20,
+      total: 0
+    };
+  },
+  methods: {
+    extractParamsFromSearchQuery(searchQuery, defaultKey) {
+      const pattern = /(?<key>\w+)=(?<keyed_terms>[^\s,]+(?:,[^\s,]+)*)|(?<isolated_term>[^"\s]+)|"(?<quoted_term>[^"]+)"/g;
+
+      let resultObj: Record<string, string | string[]> = {};
+      let match;
+
+      while ((match = pattern.exec(searchQuery)) !== null) {
+        let isolatedTerm = match.groups.isolated_term;
+        let quotedTerm = match.groups.quoted_term;
+        let key = match.groups.key;
+        let keyedTerms = match.groups.keyed_terms;
+
+        let values;
+        if (key) {
+          if (key.startsWith("in__") || key.endsWith("__in") || key == "tags" || keyedTerms.includes(",")) {
+            values = keyedTerms.split(",").map(term => term.trim());
+          } else {
+            values = keyedTerms;
+          }
+          resultObj[key] = values;
+        }
+
+        // Logging isolated and quoted terms (optional)
+        if (isolatedTerm) {
+          resultObj[defaultKey] = isolatedTerm;
+        }
+        if (quotedTerm) {
+          resultObj[defaultKey] = quotedTerm;
+        }
+      }
+      return resultObj;
+    },
+    loadOjects({ page, itemsPerPage, sortBy }: { page: number; itemsPerPage: number; sortBy: string }) {
+      let params = {
+        page: page - 1,
+        count: itemsPerPage,
+        type: this.searchSubtype,
+        query: this.extractParamsFromSearchQuery(this.searchQuery, "name")
+      };
+      axios.post(`/api/v2/${this.searchType}/search`, params).then(response => {
+        this.items = response.data.entities;
+        this.total = response.data.total;
+        this.$emit("totalUpdated", this.total);
+      });
+    }
+  }
+};
 </script>
--- yeti-frontend/src/views/EntitySearch.vue
+++ yeti-frontend/src/views/EntitySearch.vue
@@ -1,38 +1,26 @@
 <template>
-  <v-data-table-server
-    v-model:page="page"
-    :itemsLength="total"
-    :items-per-page="perPage"
-    :headers="headers"
-    density="compact"
-    :items="items"
-    @update:options="loadOjects"
-    :search="searchQuery"
-    :show-select="showSelect"
-    :item-value="item => item.id"
-    v-model="selectedObservables"
-    hover
-    class="ma-5"
-  >
-    <template v-slot:item.name="{ item }">
-      <router-link
-        :to="{ name: 'EntityDetails', params: { id: item.id } }"
-        class="text-blue"
-        style="text-decoration:none"
-        >{{ item.name }}</router-link
-      >
-    </template>
-    <template v-slot:item.tags="{ item }">
-      <v-chip
-        v-for="name in Object.keys(item.tags)"
-        :color="item.tags[name].fresh ? 'blue ' : 'red'"
-        :text="name"
-        class="mx-1"
-        label
-        size="small"
-      ></v-chip>
-    </template>
-  </v-data-table-server>
+  <v-card>
+    <v-tabs v-model="activeTab" color="primary">
+      <v-tab :value="typeDef.type" v-for="typeDef in displayedEntityTypes">
+        <v-icon size="x-large">{{ typeDef.icon }}</v-icon
+        >{{ typeDef.name }} ({{ entityCount[typeDef.type] }})
+      </v-tab>
+    </v-tabs>
+    <v-card-text>
+      <v-window v-model="activeTab">
+        <v-window-item v-for="typeDef in entityTypes" :value="typeDef.type" v-bind:key="typeDef.type" eager>
+          <object-list
+            searchType="entities"
+            :search-subtype="typeDef.type"
+            :search-query="searchQuery"
+            :headers="getFieldForType(typeDef.type)"
+            @totalUpdated="countEntities(typeDef.type, $event)"
+          />
+        </v-window-item>
+      </v-window>
+    </v-card-text>
+  </v-card>
+
   <v-navigation-drawer permament location="right" width="400" ref="drawer">
     <v-list-item class="mt-4">
       <v-text-field
@@ -49,10 +37,16 @@
 
 <script lang="ts" setup>
 import axios from "axios";
+import { ENTITY_TYPES } from "@/definitions/entityDefinitions.js";
+import ObjectList from "@/components/ObjectList.vue";
 </script>
 
 <script lang="ts">
 export default {
+  name: "entitySearch",
+  components: {
+    ObjectList
+  },
   data() {
     return {
       items: [],
@@ -65,58 +59,43 @@ export default {
       perPage: 20,
       total: 0,
       searchQuery: "",
-      showSelect: false,
-      selectedObservables: [],
-      bulkTags: [],
-      exportTemplates: [],
-      selectedExportTemplate: null
+      entityTypes: ENTITY_TYPES,
+      entityCount: ENTITY_TYPES.reduce((acc, cur) => {
+        acc[cur.type] = 0;
+        return acc;
+      }, {}),
+      activeTab: ""
     };
   },
   methods: {
-    extractParamsFromSearchQuery(searchQuery, defaultKey) {
-      const pattern = /(?<key>\w+)=(?<keyed_terms>[^\s,]+(?:,[^\s,]+)*)|(?<isolated_term>[^"\s]+)|"(?<quoted_term>[^"]+)"/g;
-
-      let resultObj: Record<string, string | string[]> = {};
-      let match;
-
-      while ((match = pattern.exec(searchQuery)) !== null) {
-        let isolatedTerm = match.groups.isolated_term;
-        let quotedTerm = match.groups.quoted_term;
-        let key = match.groups.key;
-        let keyedTerms = match.groups.keyed_terms;
-
-        let values;
-        if (key) {
-          if (key.startsWith("in__") || key.endsWith("__in") || key == "tags" || keyedTerms.includes(",")) {
-            values = keyedTerms.split(",").map(term => term.trim());
-          } else {
-            values = keyedTerms;
-          }
-          resultObj[key] = values;
-        }
-
-        // Logging isolated and quoted terms (optional)
-        if (isolatedTerm) {
-          resultObj[defaultKey] = isolatedTerm;
-        }
-        if (quotedTerm) {
-          resultObj[defaultKey] = quotedTerm;
+    countEntities(type, count) {
+      this.entityCount[type] = count;
+      if (!this.$route.hash) {
+        this.navigateToFirstPopulatedTab();
+      }
+    },
+    navigateToFirstPopulatedTab() {
+      for (const entityType of this.entityTypes) {
+        if (this.entityCount[entityType.type] > 0) {
+          this.activeTab = entityType.type;
+          break;
         }
       }
-      return resultObj;
     },
-    loadOjects({ page, itemsPerPage, sortBy }: { page: number; itemsPerPage: number; sortBy: string }) {
-      let params = {
-        page: page - 1,
-        count: itemsPerPage,
-        query: this.extractParamsFromSearchQuery(this.searchQuery, "name")
-      };
-      axios.post("http://localhost:3000/api/v2/entities/search", params).then(response => {
-        this.items = response.data.entities;
-        this.total = response.data.total;
+    getFieldForType(typeName) {
+      let fields = this.entityTypes.find(type => type.type === typeName).fields.filter(field => field.displayList);
+      return fields.map(field => {
+        return {
+          title: field.label,
+          key: field.field
+        };
       });
     }
   },
-  mounted() {}
+  computed: {
+    displayedEntityTypes() {
+      return this.entityTypes.filter(type => this.entityCount[type.type] > 0);
+    }
+  }
 };
 </script>
