4d742f68e5ebf35cb30a9f232d9feb20c8a9288f
tomchop <tomchop@gmail.com>
2023-12-01 19:59:36 +0000

    Adjust entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [[ ! -z "${VUE_API_BACKEND}" ]]; then
+  # this is the busybox version of sed
+  sed -i s/localhost/${VUE_API_BACKEND}/ vue.config.js
+fi
+
 if [[ "$1" =~ "dev" ]]; then
     if [[ ! -f ".node_installed" ]]; then
       echo ".node_installed not found, installing node modules"
@@ -8,11 +13,18 @@ if [[ "$1" =~ "dev" ]]; then
       npm install
       touch .node_installed
     fi
-    if [[ ! -z "${VUE_API_BACKEND}" ]]; then
-      # this is the busybox version of sed
-      sed -i s/localhost/${VUE_API_BACKEND}/ vue.config.js
-    fi
     npm run serve
+fi
+
+if [[ "$1" =~ "dev3" ]]; then
+    cd yeti-frontend
+    if [[ ! -f ".node_installed" ]]; then
+      echo ".node_installed not found, installing node modules"
+      rm -rf node_modules
+      npm install
+      touch .node_installed
+    fi
+    npm run dev
 fi
 
 if [[ "$1" =~ "shell" ]]; then
