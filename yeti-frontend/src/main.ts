c553533e23204a71c2326972ba4f1f3a7ec945d0
tomchop <tomchop@gmail.com>
2023-12-04 18:27:50 +0000

    Add mitt to use as an eventbus

--- yeti-frontend/src/main.ts
+++ yeti-frontend/src/main.ts
@@ -5,16 +5,21 @@
  */
 
 // Plugins
-import { registerPlugins } from '@/plugins'
+import { registerPlugins } from "@/plugins";
 
 // Components
-import App from './App.vue'
+import App from "./App.vue";
 
 // Composables
-import { createApp } from 'vue'
+import { createApp } from "vue";
 
-const app = createApp(App)
+// Eventbus
+import eventBus from "./plugins/eventbus";
+import { addHook } from "isomorphic-dompurify";
 
-registerPlugins(app)
+const app = createApp(App);
 
-app.mount('#app')
+registerPlugins(app);
+
+app.use(eventBus);
+app.mount("#app");
