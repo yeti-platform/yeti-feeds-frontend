2df73efad84cabea10af82b9ee9325b9e88f348c
tomchop <tomchop@gmail.com>
2023-12-04 18:28:04 +0000

    Add snackbar for eventbus

--- yeti-frontend/src/App.vue
+++ yeti-frontend/src/App.vue
@@ -1,9 +1,46 @@
 <template>
   <router-view />
+  <v-snackbar
+    v-model="displaySnackBar"
+    :timeout="50000"
+    variant="flat"
+    :color="snackBarStatus === 'success' ? 'green-lighten-2' : 'red-lighten-2'"
+  >
+    {{ snackBarMessage }}
+    <template v-slot:actions>
+      <v-btn
+        :color="snackBarStatus === 'success' ? 'green-darken-2' : 'red-darken-2'"
+        variant="flat"
+        rounded="2"
+        @click="displaySnackBar = false"
+      >
+        OK
+      </v-btn>
+    </template>
+  </v-snackbar>
 </template>
 
 <script lang="ts" setup></script>
 
 <script lang="ts">
-export default {};
+export default {
+  data() {
+    return {
+      displaySnackBar: false,
+      snackBarMessage: "",
+      snackBarStatus: ""
+    };
+  },
+  methods: {
+    displayMessage(event) {
+      console.log("message displayed!");
+      this.snackBarMessage = event.message;
+      this.snackBarStatus = event.status;
+      this.displaySnackBar = true;
+    }
+  },
+  created() {
+    this.$eventBus.on("displayMessage", this.displayMessage);
+  }
+};
 </script>
--- yeti-frontend/src/layouts/default/View.vue
+++ yeti-frontend/src/layouts/default/View.vue
@@ -1,42 +1,5 @@
 <template>
   <v-main class="d-flex justify-center">
-    <router-view @displayMessage="displayMessage($event)" />
-    <v-snackbar
-      v-model="displaySnackBar"
-      :timeout="50000"
-      variant="flat"
-      :color="snackBarStatus === 'success' ? 'green-lighten-2' : 'red-lighten-2'"
-    >
-      {{ snackBarMessage }}
-      <template v-slot:actions>
-        <v-btn
-          :color="snackBarStatus === 'success' ? 'green-darken-2' : 'red-darken-2'"
-          variant="flat"
-          rounded="2"
-          @click="displaySnackBar = false"
-        >
-          OK
-        </v-btn>
-      </template>
-    </v-snackbar>
+    <router-view />
   </v-main>
 </template>
-
-<script lang="ts">
-export default {
-  data() {
-    return {
-      displaySnackBar: false,
-      snackBarMessage: "",
-      snackBarStatus: ""
-    };
-  },
-  methods: {
-    displayMessage(event) {
-      this.snackBarMessage = event.message;
-      this.snackBarStatus = event.status;
-      this.displaySnackBar = true;
-    }
-  }
-};
-</script>
