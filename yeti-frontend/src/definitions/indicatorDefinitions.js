5d9c1a754f6b25b07d161229bbeed70da6c40284
Thomas Chopitea <tomchop@gmail.com>
2023-12-29 08:18:29 +0000

    Change default width to 200px

--- yeti-frontend/src/definitions/indicatorDefinitions.js
+++ yeti-frontend/src/definitions/indicatorDefinitions.js
@@ -6,7 +6,7 @@ export const INDICATOR_TYPES = [
     name: "Regular expression",
     type: "regex",
     fields: [
-      { field: "created", type: "date", label: "Created", displayList: true, editable: false, width: "190px" },
+      { field: "created", type: "date", label: "Created", displayList: true, editable: false, width: "200px" },
       { field: "name", type: "text", label: "Name", displayList: true, editable: true },
       { field: "pattern", type: "code", label: "Pattern", displayList: false, editable: true },
       { field: "relevant_tags", type: "list", label: "Relevant tags", displayList: true, editable: true },
@@ -27,7 +27,7 @@ export const INDICATOR_TYPES = [
     name: "Query",
     type: "query",
     fields: [
-      { field: "created", type: "date", label: "Created", displayList: true, editable: false, width: "190px" },
+      { field: "created", type: "date", label: "Created", displayList: true, editable: false, width: "200px" },
       { field: "name", type: "text", label: "Name", displayList: true, editable: true },
       { field: "pattern", type: "code", label: "Query text", displayList: false, editable: true },
       { field: "relevant_tags", type: "list", label: "Relevant tags", displayList: true, editable: true },
@@ -57,7 +57,7 @@ export const INDICATOR_TYPES = [
     name: "Yara",
     type: "yara",
     fields: [
-      { field: "created", type: "date", label: "Created", displayList: true, editable: false, width: "190px" },
+      { field: "created", type: "date", label: "Created", displayList: true, editable: false, width: "200px" },
       { field: "name", type: "text", label: "Name", displayList: true, editable: true },
       { field: "pattern", type: "code", label: "Rule text", displayList: false, editable: true },
       { field: "relevant_tags", type: "list", label: "Relevant tags", displayList: true, editable: true },
@@ -78,7 +78,7 @@ export const INDICATOR_TYPES = [
     name: "Sigma",
     type: "sigma",
     fields: [
-      { field: "created", type: "date", label: "Created", displayList: true, editable: false, width: "190px" },
+      { field: "created", type: "date", label: "Created", displayList: true, editable: false, width: "200px" },
       { field: "name", type: "text", label: "Name", displayList: true, editable: true },
       { field: "pattern", type: "code", label: "Rule text", displayList: false, editable: true },
       { field: "relevant_tags", type: "list", label: "Relevant tags", displayList: true, editable: true },
