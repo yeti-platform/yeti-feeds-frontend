bb6f419fc958d0c092a0ab1c9ecc4d8e8c651c8b
Thomas Chopitea <tomchop@gmail.com>
2023-12-29 09:31:03 +0000

    Popup with link description

--- yeti-frontend/src/components/RelatedObjects.vue
+++ yeti-frontend/src/components/RelatedObjects.vue
@@ -11,44 +11,45 @@
   >
     <template v-slot:item="{ item }">
       <tr>
-        <td v-for="node in item.nodeChain" v-bind:key="node.id" :class="{ object: !node.direction }">
+        <td
+          v-for="node in item.nodeChain"
+          v-bind:key="node.id"
+          :class="{ object: !node.direction, link: node.direction }"
+        >
           <span v-if="node.direction">
             <v-icon v-if="node.direction == 'out'">mdi-arrow-right</v-icon>
             <v-icon v-else-if="node.direction == 'in'">mdi-arrow-left</v-icon>
-            {{ node.type }}
+
+            <v-btn size="small" variant="text" append-icon="mdi-information" v-if="node.description">
+              <template v-slot:append>
+                <v-icon class="on-surface"></v-icon>
+              </template>
+              {{ node.type }}
+              <v-menu activator="parent">
+                <v-sheet class="px-5 py-2" color="background" width="auto" elevation="10" style="font-size: 0.8rem;">
+                  <yeti-markdown :text="node.description" />
+                </v-sheet>
+              </v-menu>
+            </v-btn>
           </span>
           <span v-else-if="node.root_type === 'observable'" class="short-links">
-            <span class="short-links">
-              <v-tooltip activator="parent" location="top" :open-delay="200">{{ node.value }}</v-tooltip>
-              <router-link :to="{ name: 'ObservableDetails', params: { id: node.id } }">
-                {{ node.value }}
-              </router-link>
-            </span>
-
-            <span v-if="fields.includes('tags')">
-              <v-chip
-                v-for="tag in Object.keys(node.tags)"
-                :key="tag"
-                :color="node.tags[tag].fresh ? 'blue' : ''"
-                class="mx-1"
-                size="x-small"
-              >
-                {{ tag }}
-              </v-chip>
-            </span>
+            <v-tooltip activator="parent" location="top" :open-delay="200">{{ node.value }}</v-tooltip>
+            <router-link :to="{ name: 'ObservableDetails', params: { id: node.id } }">
+              {{ node.value }}
+            </router-link>
           </span>
-          <span v-else-if="node.root_type === 'entity'">
-            <v-icon :icon="getIconForType(node.type)" start></v-icon>
-            <span class="short-links">
+          <span v-else-if="node.root_type === 'entity'" class="short-links">
+            <v-icon :icon="getIconForType(node.type)" start size="small"></v-icon>
+            <span>
               <v-tooltip activator="parent" location="top" :open-delay="200">{{ node.name }}</v-tooltip>
               <router-link :to="{ name: 'EntityDetails', params: { id: node.id } }">
                 {{ node.name }}
               </router-link>
             </span>
           </span>
-          <span v-else-if="node.root_type === 'indicator'">
-            <v-icon :icon="getIconForType(node.type)" start></v-icon>
-            <span class="short-links">
+          <span v-else-if="node.root_type === 'indicator'" class="short-links">
+            <v-icon :icon="getIconForType(node.type)" start size="small"></v-icon>
+            <span>
               <v-tooltip activator="parent" location="top" :open-delay="200">{{ node.name }}</v-tooltip>
               <router-link :to="{ name: 'IndicatorDetails', params: { id: node.id } }">
                 {{ node.name }}
@@ -59,7 +60,7 @@
             <v-chip> {{ node.name }}</v-chip>
           </span>
         </td>
-        <td>
+        <td class="controls" v-if="hops === 1">
           <v-btn
             icon="mdi-link-off"
             @click="unlink(item.edges[0].id)"
@@ -92,9 +93,11 @@
 
 <script lang="ts" setup>
 import axios from "axios";
+
 import { ENTITY_TYPES } from "@/definitions/entityDefinitions.js";
 import { INDICATOR_TYPES } from "@/definitions/indicatorDefinitions.js";
 import EditLink from "@/components/EditLink.vue";
+import YetiMarkdown from "@/components/YetiMarkdown.vue";
 </script>
 
 <script lang="ts">
@@ -110,7 +113,8 @@ export default {
     hops: { type: Number, default: 1 }
   },
   components: {
-    EditLink
+    EditLink,
+    YetiMarkdown
   },
   data() {
     return {
@@ -172,21 +176,6 @@ export default {
           console.log(error);
         });
     },
-    editEdge(edge) {
-      this.$buefy.modal.open({
-        parent: this,
-        component: EditLink,
-        hasModalCard: true,
-        trapFocus: true,
-        props: {
-          edge: edge,
-          vertices: this.vertices
-        },
-        events: {
-          refresh: this.fetchNeighbors
-        }
-      });
-    },
     getIconForType(type) {
       return this.objectTypes.find(objectType => objectType.type === type).icon;
     },
@@ -224,7 +213,7 @@ export default {
               nodeChain.push(edge);
               nodeChain.push(this.vertices[edge.source]);
             } else {
-              edge.direction = "ou";
+              edge.direction = "out";
               nodeChain.push(edge);
               nodeChain.push(this.vertices[edge.target]);
             }
@@ -251,5 +240,13 @@ export default {
   text-overflow: ellipsis;
   overflow: hidden;
   display: block;
+}
+
+td.controls {
+  width: 110px;
+}
+
+td.link {
+  width: 200px;
 }
 </style>
