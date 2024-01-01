360a9b22e8c7b1cb2aa63f1538a81c63c6747b2a
tomchop <tomchop@gmail.com>
2024-01-01 19:38:11 +0000

    Fix: Remove tagging control from indicator view

--- src/views/ObjectDetails.vue
+++ src/views/ObjectDetails.vue
@@ -52,7 +52,7 @@
             </tbody>
           </v-table>
         </v-card>
-        <v-card class="ma-2" variant="flat">
+        <v-card class="ma-2" variant="flat" v-if="object?.root_type !== 'indicator'">
           <v-card-title>Tags</v-card-title>
           <v-combobox
             v-model="objectTags"
