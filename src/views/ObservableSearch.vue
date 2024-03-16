92b68a1c8d145dcecbdc4f459a2c71fcb4c7b67d
Thomas Chopitea <tomchop@gmail.com>
2024-03-16 22:25:47 +0000

    Tweak pagination options (fixes #156)

--- src/views/ObservableSearch.vue
+++ src/views/ObservableSearch.vue
@@ -11,6 +11,12 @@
       :search="searchQueryDebounced"
       :show-select="showSelect"
       :item-value="item => item.id"
+      :items-per-page-options="[
+        { value: 10, title: '10' },
+        { value: 25, title: '25' },
+        { value: 50, title: '50' },
+        { value: 100, title: '100' }
+      ]"
       v-model="selectedObservables"
       hover
       class="fixed-table"
