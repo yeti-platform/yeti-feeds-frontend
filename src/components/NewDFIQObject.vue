fd248e2f4444d839e635c9912f2db38e3a086962
Thomas Chopitea <tomchop@gmail.com>
2024-03-17 00:53:43 +0100

    Component for new DFIQ objects (YAML) with validation (#158)

--- src/components/NewDFIQObject.vue
+++ src/components/NewDFIQObject.vue
@@ -1,19 +1,24 @@
 <template>
   <v-card>
-    <v-card-title>{{ localObject.name }}</v-card-title>
-    <v-card-subtitle>Editing DFIQ {{ localObject.type }}</v-card-subtitle>
+    <v-card-title>New {{ typeDefinition.name }}</v-card-title>
     <v-card-text>
-      <v-textarea class="yeti-code" label="DFIQ Yaml" auto-grow v-model="localObject.dfiq_yaml"></v-textarea>
+      <v-textarea
+        class="yeti-code"
+        label="DFIQ Yaml"
+        auto-grow
+        v-model="newDFIQYaml"
+        :loading="validatingYaml"
+      ></v-textarea>
       <div v-if="yamlValidationError === 'valid'">
         <v-alert type="success">YAML is valid!</v-alert>
       </div>
-      <div v-else-if="yamlValidationError.length > 0 && localObject.dfiq_yaml.length > 0">
+      <div v-else-if="yamlValidationError.length > 0 && newDFIQYaml.length > 0">
         <v-alert type="error"
           ><strong>Invalid YAML</strong> {{ yamlValidationError }}
           <br />
           Please revise the
-          <a :href="'https://dfiq.org/contributing/specification/#' + localObject.type" target="_blank">
-            DFIQ spec for {{ localObject.type }}
+          <a :href="'https://dfiq.org/contributing/specification/#' + typeDefinition.type" target="_blank">
+            DFIQ spec for {{ typeDefinition.name }}
           </a>
         </v-alert>
       </div>
@@ -22,7 +27,8 @@
     <v-card-actions>
       <v-btn text="Toggle full screen" color="primary" @click="toggleFullScreen"></v-btn>
       <v-spacer></v-spacer>
-      <v-btn text="Cancel" color="cancel" @click="isActive.value = false"></v-btn>
+      <v-btn text="Cancel" color="cancel" @click="$emit('close')"></v-btn>
+
       <v-btn
         :disabled="yamlValidationError !== 'valid'"
         text="Save"
@@ -32,7 +38,7 @@
       ></v-btn>
     </v-card-actions>
     <v-alert v-if="errors.length > 0" type="error">
-      Error saving DFIQ {{ localObject.type }}:
+      Error saving {{ typeDefinition.name }}:
       <ul>
         <li v-for="error in errors">
           <strong>{{ error.field }}</strong
@@ -46,6 +52,7 @@
 <script lang="ts" setup>
 import axios from "axios";
 
+import { DFIQ_TYPES, DFIQ_TEMPLATES } from "@/definitions/dfiqDefinitions.js";
 import ObjectFields from "@/components/ObjectFields.vue";
 import _ from "lodash";
 </script>
@@ -54,39 +61,29 @@ import _ from "lodash";
 export default {
   components: { ObjectFields },
   props: {
-    object: {
-      type: Object,
-      default: () => {}
-    },
-    isActive: {
-      type: Object,
-      default: () => {}
+    objectType: {
+      type: String,
+      default: () => ""
     }
   },
   data() {
     return {
-      localObject: { ...this.object },
+      newObject: {},
+      newDFIQYaml: this.getTemplateForType(this.objectType),
+      yamlValidationError: "",
       errors: [],
       fullScreen: false,
-      typeToEndpointMapping: {
-        entity: "entities",
-        observable: "observables",
-        indicator: "indicators",
-        dfiq: "dfiq"
-      },
-      validatingYaml: false,
-      yamlValidationError: ""
+      validatingYaml: false
     };
   },
-  mounted() {},
   methods: {
     validateDFIQYaml() {
       this.validatingYaml = "primary";
       axios
         .post(`/api/v2/dfiq/validate`, {
-          dfiq_type: this.localObject.type,
-          dfiq_yaml: this.localObject.dfiq_yaml,
-          check_id: false
+          dfiq_type: this.objectType,
+          dfiq_yaml: this.newDFIQYaml,
+          check_id: true
         })
         .then(response => {
           if (!response.data.valid) {
@@ -98,23 +95,24 @@ export default {
         });
     },
     saveObject() {
-      let patchRequest = {
-        dfiq_type: this.localObject.type,
-        dfiq_yaml: this.localObject.dfiq_yaml
+      let request = {
+        type: this.objectType
       };
-
       axios
-        .patch(`/api/v2/dfiq/${this.localObject.id}`, patchRequest)
+        .post(`/api/v2/dfiq/from_yaml`, {
+          dfiq_type: this.objectType,
+          dfiq_yaml: this.newDFIQYaml
+        })
         .then(response => {
-          this.$eventBus.emit("displayMessage", {
-            message: "DFIQ object succesfully updated",
-            status: "success"
-          });
-          this.$emit("success", response.data);
-          this.isActive.value = false;
+          this.$eventBus.emit("displayMessage", { message: `New ${this.objectType} created`, status: "success" });
+          this.$router.push({ path: `/dfiq/${response.data.id}` });
         })
         .catch(error => {
           console.log(error);
+          if (!Array.isArray(error.response.data.detail)) {
+            this.errors = [{ field: "dfiq_yaml", message: error.response.data.detail }];
+            return;
+          }
           this.errors = error.response.data.detail
             .filter(detail => detail.loc[1] !== "type")
             .map(detail => {
@@ -126,10 +124,28 @@ export default {
     toggleFullScreen() {
       this.fullScreen = !this.fullScreen;
       this.$emit("toggle-fullscreen", this.fullScreen);
+    },
+    getTemplateForType(type) {
+      return DFIQ_TEMPLATES[type];
+    }
+  },
+  computed: {
+    typeDefinition() {
+      return DFIQ_TYPES.find(t => t.type === this.objectType);
+    },
+    editableFields() {
+      return this.typeDefinition.fields.filter(field => field.editable);
+    },
+    objectRootType() {
+      if (DFIQ_TYPES.find(t => t.type === this.objectType)) {
+        return "dfiq";
+      } else {
+        return "unknown";
+      }
     }
   },
   watch: {
-    "localObject.dfiq_yaml": _.debounce(function () {
+    newDFIQYaml: _.debounce(function () {
       this.validateDFIQYaml();
     }, 500)
   }
