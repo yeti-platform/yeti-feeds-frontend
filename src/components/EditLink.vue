93225b5ec225f88369c3d36fe818dd7281293bc2
Thomas Chopitea <tomchop@gmail.com>
2024-05-01 09:57:40 +0200

    Guide user through selecting the right links (#171)

--- src/components/EditLink.vue
+++ src/components/EditLink.vue
@@ -6,8 +6,29 @@
       {{ vertices[edge.target].value || vertices[edge.target].name }}</template
     >
     <v-card-text>
-      <v-text-field label="Type" v-model="localEdge.type"></v-text-field>
+      <!-- <v-text-field label="Type" v-model="localEdge.type"></v-text-field> -->
+      <v-combobox label="Type" v-model="localEdge.type" :items="getLinkTypeSuggestions"></v-combobox>
       <v-textarea label="Description" v-model="localEdge.description"></v-textarea>
+      <div class="d-flex justify-center">
+        <table>
+          <tr>
+            <td>
+              <v-chip
+                :text="vertices[edge.source].name || vertices[edge.source].value"
+                :prepend-icon="getIconForType(vertices[edge.source].type)"
+              />
+            </td>
+            <td>→</td>
+            <td>
+              <code>{{ localEdge.type || "?" }}</code>
+            </td>
+            <td>→</td>
+            <td>
+              <v-chip :text="vertices[edge.target].name" :prepend-icon="getIconForType(vertices[edge.target].type)" />
+            </td>
+          </tr>
+        </table>
+      </div>
     </v-card-text>
 
     <v-card-actions>
@@ -22,6 +43,12 @@
 
 <script lang="ts" setup>
 import axios from "axios";
+
+import { ENTITY_TYPES } from "@/definitions/entityDefinitions.js";
+import { INDICATOR_TYPES } from "@/definitions/indicatorDefinitions.js";
+import { OBSERVABLE_TYPES } from "@/definitions/observableDefinitions.js";
+
+import { LINK_SUGGESTIONS } from "@/definitions/linkSuggestions.js";
 </script>
 
 <script lang="ts">
@@ -71,17 +98,42 @@ export default {
         .then(response => {
           this.edge["source"] = response.data["source"];
           this.edge["target"] = response.data["target"];
+          this.localEdge = { ...this.edge };
           this.$eventBus.emit("displayMessage", { message: "Link direction swapped succesfully!", status: "success" });
         })
         .catch(error => {
           this.error = error;
           console.log(error);
         });
+    },
+    getIconForType(type) {
+      return (
+        ENTITY_TYPES.find(t => t.type === type) ||
+        INDICATOR_TYPES.find(t => t.type === type) ||
+        OBSERVABLE_TYPES.find(t => t.type === type)
+      ).icon;
     }
   },
   computed: {
     objectType() {
       return this.objectTypes.find(type => type.type === this.objectTypeName);
+    },
+    getOutgoingLinkTypeSuggestions() {
+      let source = this.vertices[this.edge.source];
+      let target = this.vertices[this.edge.target];
+      return (LINK_SUGGESTIONS[source.type] || LINK_SUGGESTIONS[source.root_type])
+        .filter(suggestion => suggestion.targets.includes(target.type) || suggestion.targets.includes(target.type))
+        .map(suggestion => suggestion.verb);
+    },
+    getIncomingLinkTypeSuggestions() {
+      let source = this.vertices[this.edge.source];
+      let target = this.vertices[this.edge.target];
+      return (LINK_SUGGESTIONS[target.type] || LINK_SUGGESTIONS[target.root_type])
+        .filter(suggestion => suggestion.targets.includes(source.type) || suggestion.targets.includes(source.type))
+        .map(suggestion => suggestion.verb);
+    },
+    getLinkTypeSuggestions() {
+      return [...new Set(this.getOutgoingLinkTypeSuggestions.concat(this.getIncomingLinkTypeSuggestions))];
     }
   }
 };
---
+++ src/definitions/linkSuggestions.js
@@ -0,0 +1,104 @@
+export const LINK_SUGGESTIONS = {
+  "attack-pattern": [
+    { verb: "delivers", targets: ["malware"] },
+    { verb: "targets", targets: ["identity", "location", "vulnerability"] },
+    { verb: "uses", targets: ["malware", "tool"] }
+  ],
+  campaign: [
+    { verb: "attributed-to", targets: ["intrusion-set", "threat-actor"] },
+    { verb: "compromises", targets: ["infrastructure"] },
+    { verb: "originates-from", targets: ["location"] },
+    { verb: "targets", targets: ["identity", "location", "vulnerability"] },
+    {
+      verb: "uses",
+      targets: ["attack-pattern", "infrastructure", "malware", "tool"]
+    }
+  ],
+  "course-of-action": [
+    { verb: "investigates", targets: ["indicator"] },
+    {
+      verb: "mitigates",
+      targets: ["attack-pattern", "indicator", "malware", "tool", "vulnerability"]
+    },
+    { verb: "remediates", targets: ["malware", "vulnerability"] }
+  ],
+  identity: [{ verb: "logated-at", targets: ["location"] }],
+  indicator: [
+    {
+      verb: "indicates",
+      targets: ["attack-pattern", "campaign", "infrastructure", "intrusion-set", "malware", "threat-actor", "tool"]
+    },
+    { verb: "based-on", targets: ["observable"] }
+  ],
+  infrastructure: [
+    { verb: "communicates-with", targets: ["infrastructure"] },
+    { verb: "consists-of", targets: ["infrastructure", "observable"] },
+    { verb: "controls", targets: ["infrastructure", "malware"] },
+    { verb: "delivers", targets: ["malware"] },
+    { verb: "has", targets: ["vulnerability"] },
+    { verb: "hosts", targets: ["tool", "malware"] },
+    { verb: "located-at", targets: ["location"] },
+    { verb: "infrastructure", targets: ["infrastructure"] }
+  ],
+  investigation: [{ verb: "linked-to", targets: [] }],
+  "intrusion-set": [
+    { verb: "attributed-to", targets: ["threat-actor"] },
+    { verb: "compromises", targets: ["infrastructure"] },
+    { verb: "hosts", targets: ["infrastructure"] },
+    { verb: "owns", targets: ["infrastructure"] },
+    { verb: "originates-from", targets: ["location"] },
+    { verb: "targets", targets: ["identity", "location", "vulnerability"] },
+    {
+      verb: "uses",
+      targets: ["attack-pattern", "infrastructure", "malware", "tool"]
+    }
+  ],
+  location: [],
+  malware: [
+    { verb: "authored-by", targets: ["threat-actor", "intrusion-set"] },
+    { verb: "beacons-to", targets: ["infrastructure"] },
+    { verb: "exfiltrates-to", targets: ["infrastructure"] },
+    {
+      verb: "communicates-with",
+      targets: ["ipv4", "ipv6", "cidr", "hostname", "url"]
+    },
+    { verb: "controls", targets: ["malware"] },
+    { verb: "downloads", targets: ["malware", "tool", "file"] },
+    { verb: "drops", targets: ["malware", "tool", "file"] },
+    { verb: "exploits", targets: ["vulnerability"] },
+    { verb: "originates-from", targets: ["location"] },
+    {
+      verb: "targets",
+      targets: ["identity", "infrastructure", "location", "vulnerability"]
+    },
+    {
+      verb: "uses",
+      targets: ["attack-pattern", "infrastructure", "malware", "tool"]
+    },
+    { verb: "variant-of", targets: ["malware"] }
+  ],
+  "threat-actor": [
+    { verb: "attributed-to", targets: ["identity"] },
+    { verb: "compromises", targets: ["infrastructure"] },
+    { verb: "hosts", targets: ["infrastructure"] },
+    { verb: "owns", targets: ["infrastructure"] },
+    { verb: "impersonates", targets: ["identity"] },
+    { verb: "located-at", targets: ["location"] },
+    { verb: "targets", targets: ["identity", "location", "vulnerability"] },
+    {
+      verb: "uses",
+      targets: ["attack-pattern", "infrastructure", "malware", "tool"]
+    }
+  ],
+  tool: [
+    { verb: "delivers", targets: ["malware"] },
+    { verb: "drops", targets: ["malware"] },
+    { verb: "has", targets: ["vulnerability"] },
+    {
+      verb: "targets",
+      targets: ["identity", "infrastructure", "location", "vulnerability"]
+    },
+    { verb: "uses", targets: ["infrastructure"] }
+  ],
+  vulnerability: []
+};
