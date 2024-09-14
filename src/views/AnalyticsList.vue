58d62f760663a3ccefff97379f686c30690fa624
Thomas Chopitea <tomchop@gmail.com>
2024-09-14 17:12:50 +0900

    Analytics display cleanup (#212)

--- src/views/AnalyticsList.vue
+++ src/views/AnalyticsList.vue
@@ -8,14 +8,14 @@
       title="One-shot"
       subtitle="Analytics that are triggered manually on specific observable types."
     >
-      <task-list task-type="oneshot"> </task-list>
+      <task-list task-type="oneshot" :display-columns="['name', 'acts_on', 'description', 'status', 'toggle']">
+      </task-list>
     </v-card>
     <v-card
       class="ma-5"
       title="Inline"
       subtitle="Analytics that trigger when an observable is added to the Yeti database."
     >
-      <task-list task-type="inline"> </task-list>
     </v-card>
   </div>
 </template>
