e295eac762aeec7f26090bc627bc4e670ee546da
Thomas Chopitea <tomchop@gmail.com>
2025-02-12 15:40:19 +0000

    Fix patching role

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -24,7 +24,7 @@
                         item-value="value"
                         density="compact"
                         :return-object="false"
-                        @update:modelValue="saveUserSettings"
+                        @update:modelValue="updateUserRole"
                         :disabled="!user.admin"
                         :hide-details="true"
                       ></v-combobox>
@@ -146,9 +146,12 @@ export default {
           this.newPassword = null;
         });
     },
-    saveUserSettings() {
+    updateUserRole() {
       axios
-        .patch(`/api/v2/users/${this.profile.id}`, this.profile)
+        .patch(`/api/v2/users/role`, {
+          role: this.profile.global_role,
+          user_id: this.profile.id
+        })
         .then(() => {
           this.$eventBus.emit("displayMessage", {
             message: "Settings successfully updated.",
