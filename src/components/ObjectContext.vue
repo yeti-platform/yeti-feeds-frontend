1c7b277c5774424e0606d409ea5ec33d11ca8955
Thomas Chopitea <tomchop@gmail.com>
2025-03-09 01:15:15 +0000

    Use expansion panels

--- src/components/ObjectContext.vue
+++ src/components/ObjectContext.vue
@@ -1,58 +1,58 @@
 <template>
-  <v-sheet class="ma-2">
-    <v-card>
-      <v-card-title class="edit-ctx-title">
-        Context entries
-        <v-chip
-          label
-          color="green"
-          v-for="source in new Set(context.map(c => c.source))"
-          v-bind:key="source"
-          size="small"
-          density="comfortable"
-          class="me-2"
-        >
-          {{ source }}
-        </v-chip>
-
-        <v-btn size="small" variant="outlined" @click="showCtxDetails = !showCtxDetails"
-          >{{ showCtxDetails ? "Hide" : "Show" }} details</v-btn
-        >
-        <v-dialog height="70%">
-          <template v-slot:activator="{ props }">
-            <v-btn v-bind="props" class="ms-2 edit-ctx-btn" size="small" variant="text"
-              ><v-icon class="me-2">mdi-pencil</v-icon>edit</v-btn
-            >
-          </template>
-          <template v-slot:default="{ isActive }">
-            <v-card max-height="100%">
-              <v-card-title class="text-h6">Edit context</v-card-title>
-              <v-card-text>
-                <v-textarea class="yeti-code" v-model="jsonContext" v-on:input="checkJson" rows="1" auto-grow />
-                <v-alert type="error" v-if="jsonError !== ''">Invalid JSON: {{ jsonError }}</v-alert>
-              </v-card-text>
-              <v-card-actions>
-                <v-btn color="cancel" variant="tonal" @click="isActive.value = false">Cancel</v-btn>
-                <v-btn color="primary" variant="tonal" @click="updateContext(isActive)">Save</v-btn>
-              </v-card-actions>
-            </v-card>
-          </template>
-        </v-dialog>
-      </v-card-title>
-      <v-treeview
-        :items="ContextTreeView()"
-        item-value="id"
-        activatable
-        open-on-click
-        slim
-        density="compact"
-        max-height="300"
-        width="100%"
-        v-if="showCtxDetails"
-      >
-      </v-treeview>
-    </v-card>
-  </v-sheet>
+  <v-card class="ma-2">
+    <v-expansion-panels>
+      <v-expansion-panel>
+        <v-expansion-panel-title class="edit-ctx-title break-title">
+          <p class="text-subtitle-1">Context entries</p>
+          <v-chip
+            label
+            color="green"
+            v-for="source in new Set(context.map(c => c.source))"
+            v-bind:key="source"
+            size="small"
+            density="comfortable"
+            class="me-2"
+          >
+            {{ source }}
+          </v-chip>
+          <v-dialog height="90%">
+            <template v-slot:activator="{ props }">
+              <v-btn v-bind="props" class="ms-2 edit-ctx-btn" size="small" variant="text" density="comfortable"
+                ><v-icon class="me-2">mdi-pencil</v-icon>edit</v-btn
+              >
+            </template>
+            <template v-slot:default="{ isActive }">
+              <v-card max-height="100%">
+                <v-card-title class="text-h6">Edit context</v-card-title>
+                <v-card-text>
+                  <v-textarea class="yeti-code" v-model="jsonContext" v-on:input="checkJson" rows="1" auto-grow />
+                  <v-alert type="error" v-if="jsonError !== ''">Invalid JSON: {{ jsonError }}</v-alert>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn color="cancel" variant="tonal" @click="isActive.value = false">Cancel</v-btn>
+                  <v-btn color="primary" variant="tonal" @click="updateContext(isActive)">Save</v-btn>
+                </v-card-actions>
+              </v-card>
+            </template>
+          </v-dialog>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          <v-treeview
+            :items="ContextTreeView()"
+            item-value="id"
+            activatable
+            open-on-click
+            slim
+            density="compact"
+            max-height="300"
+            width="100%"
+            v-if="showCtxDetails"
+          >
+          </v-treeview>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-card>
 </template>
 
 <script>
@@ -75,7 +75,7 @@ export default {
     return {
       jsonContext: JSON.stringify(this.context, null, 2),
       jsonError: "",
-      showCtxDetails: false
+      showCtxDetails: true
     };
   },
   // watch: {
